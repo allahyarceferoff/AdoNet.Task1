ba4033453a6c8656d520028e16ebbf10b9c0d976
Cefer_hg45 <Cefer_hg45@STHQ0118-10>
2023-06-23 08:47:40 +0400

    Add project files.

---
+++ WpfApp3/Repository/Repo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp3.Models;
+
+namespace WpfApp3.Repository
+{
+    public class Repo
+    {
+        SqlConnection conn;
+        string cs = ConfigurationManager.ConnectionStrings["myConn"].ConnectionString;
+        DataSet set = new DataSet();
+
+        public Repo()
+        {
+            conn = new SqlConnection();
+
+            using (conn = new SqlConnection())
+            {
+                var da = new SqlDataAdapter();
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Authors", conn);
+
+                da.SelectCommand = command;
+                da.Fill(set, "AuthorsSet");
+            }
+        }
+
+        public DataSet GetAll()
+        {
+            return set;
+        }
+
+        public void Insert(int id, string firstName, string lastName)
+        {
+            using (conn = new SqlConnection())
+            {
+                var command = new SqlCommand("INSERT INTO Authors(Id,FirstName,LastName) VALUES(@id,@firstName,@lastName)", conn);
+                conn.ConnectionString = cs;
+                conn.Open();
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    DbType = DbType.Int32,
+                    ParameterName = "@id",
+                    Value = id
+                });
+
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@firstName",
+                    Value = firstName
+                });
+                
+                command.Parameters.Add(new SqlParameter
+                {
+                    SqlDbType = SqlDbType.NVarChar,
+                    ParameterName = "@lastName",
+                    Value = lastName
+                });
+
+                var da = new SqlDataAdapter();
+                da.InsertCommand= command;
+                da.InsertCommand.ExecuteNonQuery();
+                da.Update(set, "AuthorsSet");
+                set.Clear();
+
+                da = new SqlDataAdapter("SELECT * FROM Authors", conn);
+
+                da.Fill(set, "AuthorsSet");
+
+
+            }
+        }
+    }
+}
---
+++ WpfApp3/ViewModels/MainWindowViewModels.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp3.Commands;
+using WpfApp3.Models;
+using WpfApp3.Repository;
+
+namespace WpfApp3.ViewModels
+{
+    public class MainWindowViewModels : BaseViewModel
+    {
+        public DataSet AutorSet { get; set; } = new DataSet();
+
+        public Repo AuthorsRepo { get; set; }
+        public RelayCommand InsertCommand { get; set; }
+        public RelayCommand DeleteCommand { get; set; }
+        public RelayCommand UpdateCommand { get; set; }
+        public RelayCommand ShowAllCommand { get; set; }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string firstName;
+
+        public string Firstname
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged(); }
+        }
+
+        private string lastName;
+
+        public string Lastname
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged(); }
+        }
+
+
+        public MainWindowViewModels()
+        {
+            AuthorsRepo = new Repo();
+            AutorSet = AuthorsRepo.GetAll();
+            
+
+            InsertCommand = new RelayCommand((obj) =>
+            {
+                AuthorsRepo.Insert(Id, Firstname, Lastname);
+            });
+
+        }
+    }
+}
